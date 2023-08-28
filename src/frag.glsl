c7722b448119a9370e99986f44d9ed5124e12402
Zakhar Tarshis <ztarshis@tradingview.com>
2023-08-28 15:25:37 +0300

    Clean up shaders

--- src/frag.glsl
+++ src/frag.glsl
@@ -5,10 +5,6 @@ in vec2 frag_uv;
 uniform sampler2D tex;
 uniform float opacity;
 
-vec4 blend_normal(vec4 d, vec4 s);
-vec4 blend_add(vec4 d, vec4 s);
-vec4 blend_multi(vec4 d, vec4 s);
-
 void main() {
 	vec4 texture = texture(tex, frag_uv);
 	vec4 color = vec4(
@@ -18,20 +14,3 @@ void main() {
 	gl_FragColor = color;
 }
 
-vec4 blend_normal(vec4 d, vec4 s) {
-	vec3 rgb = d.xyz * (1 - s.w) + s.xyz;
-	float a = d.w * (1 - s.w) + s.w;
-	return vec4(rgb, a);
-}
-
-vec4 blend_add(vec4 d, vec4 s) {
-	vec3 rgb = d.xyz + s.xyz;
-	float a = d.w;
-	return vec4(rgb, a);
-}
-
-vec4 blend_multi(vec4 d, vec4 s) {
-	vec3 rgb = d.xyz * (1 - s.w) + s.xyz * d.xyz;
-	float a = d.w;
-	return vec4(rgb, a);
-}
--- src/vert.glsl
+++ src/vert.glsl
@@ -5,12 +5,11 @@ in vec2 texture_uv;
 out vec2 frag_uv;
 
 uniform vec2 size;
-uniform vec2 origin;
 uniform float scale;
 
 void main() {
 	frag_uv = texture_uv;
-	vec2 pos = position / 10;
+	vec2 pos = 2 * scale * position / size;
 	gl_Position = vec4(pos, 0.0, 1.0);
 }
 
