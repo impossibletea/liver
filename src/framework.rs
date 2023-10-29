ba329b5ec97e1334d12faf7014322d734d983b2b
impossibleTea <impossibletea@icloud.com>
2023-10-29 15:38:13 +0300

    Add proper motion queue control

--- src/framework.rs
+++ src/framework.rs
@@ -1,7 +1,7 @@
 use std::{
     fs::File,
     iter::zip,
-    collections::HashMap,
+    collections::{HashMap, VecDeque},
 };
 use glium::{
     Blend,
@@ -21,13 +21,12 @@ use cubism::{
     core,
     motion::Motion,
     model::UserModel,
-    json::model::Model3,
+    json::{
+        model::Model3,
+        motion::Motion3,
+    },
 };
-use crate::{
-    Config,
-    APP_NAME,
-    CONFIG,
-};
+use crate::{Config, APP_NAME, CONFIG};
 
 //  __  __           _      _
 // |  \/  | ___   __| | ___| |
@@ -36,24 +35,45 @@ use crate::{
 // |_|  |_|\___/ \__,_|\___|_|
 
 pub struct Model {
-    //name:      String,
-    //path:      PathBuf,
     model:     UserModel,
-    motions:   HashMap<String, Motion>,
-    current:   String,
+    motions:   HashMap<String, MotionData>,
+    queue:     Queue,
     canvas:    CanvasInfo,
     textures:  Vec<SrgbTexture2d>,
     drawables: Vec<Drawable>,
 }
 
-struct Drawable {
-    vertex_buffer: VertexBuffer<Vert>,
-    index_buffer:  IndexBuffer<u16>,
-    opacity:       f32,
-    texture_index: i32,
-    render_order:  i32,
+//  __  __       _   _             ____        _        
+// |  \/  | ___ | |_(_) ___  _ __ |  _ \  __ _| |_ __ _ 
+// | |\/| |/ _ \| __| |/ _ \| '_ \| | | |/ _` | __/ _` |
+// | |  | | (_) | |_| | (_) | | | | |_| | (_| | || (_| |
+// |_|  |_|\___/ \__|_|\___/|_| |_|____/ \__,_|\__\__,_|
+                                                     
+struct MotionData {
+    motion:   Motion,
+    looped:   bool,
+    duration: f32,
 }
 
+//   ___                        
+//  / _ \ _   _  ___ _   _  ___ 
+// | | | | | | |/ _ \ | | |/ _ \
+// | |_| | |_| |  __/ |_| |  __/
+//  \__\_\\__,_|\___|\__,_|\___|
+                             
+struct Queue {
+    lineup:   VecDeque<String>,
+    current:  Option<String>,
+    duration: f32,
+    elapsed:  f32,
+}
+
+//   ____                          ___        __       
+//  / ___|__ _ _ ____   ____ _ ___|_ _|_ __  / _| ___  
+// | |   / _` | '_ \ \ / / _` / __|| || '_ \| |_ / _ \ 
+// | |__| (_| | | | \ V / (_| \__ \| || | | |  _| (_) |
+//  \____\__,_|_| |_|\_/ \__,_|___/___|_| |_|_|  \___/ 
+                                                    
 struct CanvasInfo {
     size:   [f32; 2],
     origin: [f32; 2],
@@ -138,27 +158,44 @@ impl Model {
                 None    => {eprintln!("Fucked up motion name"); continue}
             };
 
-            let motion = Motion::from_motion3_json(path
-                                                   .join(&m.file));
+            if motions.contains_key(&n) {
+                eprintln!("Duplicated motion {n}");
+                continue;
+            }
 
-            let m = match motion {
-                Ok(r)  => r,
-                Err(e) => {eprintln!("Failed to load motion: {e}"); continue}
+            let motion3 = 
+                File::open(path.join(&m.file))
+                .map_err(|e| format!("Error opening json: {e}"))
+                .and_then(|f| {
+                    Motion3::from_reader(f)
+                    .map_err(|e| format!("Error parsing json: {e}"))
+                })?;
+
+            let looped   = motion3.meta.looped;
+            let duration = motion3.meta.duration;
+            let motion   = Motion::new(motion3);
+
+            let m = MotionData {
+                looped,
+                duration,
+                motion,
             };
 
-            match motions.insert(n.clone(), m) {
-                Some(_) => eprintln!("Duplicated motion {n}"),
-                None    => eprintln!("Loaded motion {n}")
-            }
+            motions.insert(n, m);
         }
 
-        //                                  _   
-        //    ___ _   _ _ __ _ __ ___ _ __ | |_ 
-        //   / __| | | | '__| '__/ _ \ '_ \| __|
-        //  | (__| |_| | |  | | |  __/ | | | |_ 
-        // (_)___|\__,_|_|  |_|  \___|_| |_|\__|
+        //    __ _ _   _  ___ _   _  ___ 
+        //   / _` | | | |/ _ \ | | |/ _ \
+        //  | (_| | |_| |  __/ |_| |  __/
+        // (_)__, |\__,_|\___|\__,_|\___|
+        //      |_|                      
 
-        let current = "idle".to_string();
+        let queue = Queue {
+            lineup:   VecDeque::new(),
+            current:  None,
+            duration: 0.,
+            elapsed:  0.,
+        };
 
         //    ___ __ _ _ ____   ____ _ ___
         //   / __/ _` | '_ \ \ / / _` / __|
@@ -207,10 +244,8 @@ impl Model {
         // (_)__,_|_|  \__,_| \_/\_/ \__,_|_.__/|_|\___||___/
 
         let mut drawables: Vec<_> =
-            Result::from_iter(
-                model.drawables()
-                .map(|d| Drawable::new(d, display))
-            )?;
+            Result::from_iter(model.drawables()
+                              .map(|d| Drawable::new(d, display)))?;
         drawables.sort_unstable_by_key(|d| d.render_order);
 
         //           _
@@ -220,11 +255,9 @@ impl Model {
         // |_|  \___|\__|\__,_|_|  |_| |_|
 
         Ok(Self {
-            //name,
-            //path,
             model,
             motions,
-            current,
+            queue,
             canvas,
             textures,
             drawables,
@@ -286,15 +319,31 @@ impl Model {
     pub fn update(&mut     self,
                   dt:      f64,
                   display: &Display) -> Result<(), String> {
-        self.motions
-        .get_mut(&self.current)
-        .ok_or(format!("No motion {}", self.current))
-        .and_then(|m| {
-            //m.set_looped(true);
-            m.tick(dt);
-            m.update(self.model.model_mut())
-            .map_err(|e| format!("Failed to update model: {e}"))
-        })?;
+        let queue = &mut self.queue;
+        queue.elapsed += dt as f32;
+
+        if queue.elapsed >= queue.duration {
+            self.next()
+            .or(self.set_motion("idle"))
+            .ok_or(format!("Queue fucked up"))?;
+        }
+
+        let current = match &self.queue.current {
+            Some(c) => c,
+            None    => return Err(format!("No motion set"))
+        };
+
+        let mut motion_data =
+            &mut self.motions
+            .get_mut(current)
+            .ok_or(format!("No motion {current}"))?;
+
+        let motion = &mut motion_data.motion;
+        motion.tick(dt);
+        motion
+        .update(self.model.model_mut())
+        .map_err(|e| format!("Failed to update model: {e}"))?;
+
         self.model.model_mut().update();
 
         let mut drawables: Vec<_> =
@@ -316,11 +365,15 @@ impl Model {
     // (_|_) | .__/|_|\__,_|\__, |
     //       |_|            |___/ 
 
-    pub fn play(&mut self) {
+    pub fn play(&mut self) -> Option<()> {
+        let current = match &self.queue.current {
+            Some(c) => c,
+            None    => return None
+        };
+
         self.motions
-        .get_mut(&self.current)
-        .map(|m| m.play())
-        .unwrap_or(())
+        .get_mut(current)
+        .map(|m| m.motion.play())
     }
 
     //  _ _   _ __   __ _ _   _ ___  ___ 
@@ -329,12 +382,42 @@ impl Model {
     // (_|_) | .__/ \__,_|\__,_|___/\___|
     //       |_|                         
 
-    pub fn pause(&mut self) {
+    pub fn pause(&mut self) -> Option<()> {
+        let current = match &self.queue.current {
+            Some(c) => c,
+            None    => return None
+        };
+
         self.motions
-        .get_mut(&self.current)
-        .map(|m| m.pause())
-        .unwrap_or(())
+        .get_mut(current)
+        .map(|m| m.motion.pause())
     }
+
+    //            _              
+    //  _ _   ___| |_ ___  _ __  
+    // (_|_) / __| __/ _ \| '_ \ 
+    //  _ _  \__ \ || (_) | |_) |
+    // (_|_) |___/\__\___/| .__/ 
+    //                    |_|    
+
+    pub fn stop(&mut self) -> Option<()> {
+        let current = match &self.queue.current {
+            Some(c) => c,
+            None    => return None
+        };
+
+        self.motions
+        .get_mut(current)
+        .map(|m| m.motion.stop())
+    }
+
+    //                      _             _   
+    //  _ _   _ __ ___  ___| |_ __ _ _ __| |_ 
+    // (_|_) | '__/ _ \/ __| __/ _` | '__| __|
+    //  _ _  | | |  __/\__ \ || (_| | |  | |_ 
+    // (_|_) |_|  \___||___/\__\__,_|_|   \__|
+
+    pub fn restart(&mut self) -> Option<()> {self.stop().and(self.play())}
 
     //                 _                     _   _             
     //  _ _   ___  ___| |_   _ __ ___   ___ | |_(_) ___  _ __  
@@ -342,14 +425,70 @@ impl Model {
     //  _ _  \__ \  __/ |_  | | | | | | (_) | |_| | (_) | | | |
     // (_|_) |___/\___|\__| |_| |_| |_|\___/ \__|_|\___/|_| |_|
     
-    pub fn set_motion(&mut   self,
-                      index: usize) -> Option<usize> {
-        //if index >= self.motions.len() {return None};
-        //self.current = index;
-        //self.play();
-        //Some(self.current)
-        None
+    fn set_motion(&mut self,
+                  new: &str) -> Option<()> {
+        self.stop().unwrap_or(());
+
+        if !self.motions.contains_key(new) {return None;}
+
+        let current = new.to_string();
+        let motion_data = &mut self.motions.get_mut(&current)?;
+
+        motion_data.motion.set_looped(motion_data.looped);
+        motion_data.motion.play();
+
+        self.queue.current = Some(new.to_string());
+        self.queue.duration = motion_data.duration;
+        self.queue.elapsed = 0.;
+        Some(())
     }
+
+    //  _ _    __ _ _   _  ___ _   _  ___ 
+    // (_|_)  / _` | | | |/ _ \ | | |/ _ \
+    //  _ _  | (_| | |_| |  __/ |_| |  __/
+    // (_|_)  \__, |\__,_|\___|\__,_|\___|
+    //           |_|                      
+
+    pub fn queue(&mut self,
+                 new: &str) -> Option<()> {
+        if !self.motions.contains_key(new) {return None;}
+
+        if self.queue.current.is_some() {
+            self.queue.lineup.push_back(new.to_string());
+        } else {
+            self.set_motion(new);
+        }
+        Some(())
+    }
+
+    //                        _   
+    //  _ _   _ __   _____  _| |_ 
+    // (_|_) | '_ \ / _ \ \/ / __|
+    //  _ _  | | | |  __/>  <| |_ 
+    // (_|_) |_| |_|\___/_/\_\\__|
+
+    fn next(&mut self) -> Option<()> {
+        self.queue.lineup
+        .pop_front()
+        .map(|c| {
+            self.set_motion(c.as_str());
+            self.queue.elapsed = 0.;
+        })
+    }
+}
+
+//  ____                          _     _      
+// |  _ \ _ __ __ ___      ____ _| |__ | | ___ 
+// | | | | '__/ _` \ \ /\ / / _` | '_ \| |/ _ \
+// | |_| | | | (_| |\ V  V / (_| | |_) | |  __/
+// |____/|_|  \__,_| \_/\_/ \__,_|_.__/|_|\___|
+                                            
+struct Drawable {
+    vertex_buffer: VertexBuffer<Vert>,
+    index_buffer:  IndexBuffer<u16>,
+    opacity:       f32,
+    texture_index: i32,
+    render_order:  i32,
 }
 
 impl Drawable {
