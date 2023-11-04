61ef532f32b444643b690645522ded3f95520e11
impossibleTea <impossibletea@icloud.com>
2023-11-04 19:00:16 +0300

    Account for canvas origin when rendering

--- src/vert.glsl
+++ src/vert.glsl
@@ -11,7 +11,8 @@ uniform vec2 aspect;
 
 void main() {
 	frag_uv = texture_uv;
-	vec2 pos = 2 * scale * position / size;
+	vec2 pos = 1 + 2 * (scale * position - origin) / size;
+	//pos -= origin;
 	pos *= aspect;
 	gl_Position = vec4(pos, 0.0, 1.0);
 }
